22c974c53baacd15714590efe8ed696f924fe9b3
unknown <patrickviegas4@gmail.com>
2021-10-21 20:46:47 -0300

    criado authprovider

--- nlw_2021/web/src/components/LoginBox/index.tsx
+++ nlw_2021/web/src/components/LoginBox/index.tsx
@@ -1,12 +1,50 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc'
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=24482bb15f1a3e3ea51c`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+
+    console.log(user);
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGitHubCode = url.includes('?code=');
+
+    if (hasGitHubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, [])
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGitHub}>
+      <a href={signInUrl} className={styles.signInWithGitHub}>
         <VscGithubInverted size="24" />
         Entrar com GitHub
       </a>
--- nlw_2021/web/src/components/MessageList/index.tsx
+++ nlw_2021/web/src/components/MessageList/index.tsx
@@ -29,36 +29,20 @@ export function MessageList() {
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
 
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
-            </div>
-            <span>Diego Fernandes</span>
-          </div>
-        </li>
 
       </ul>
     </div>
